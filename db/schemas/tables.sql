8f1712ff8d5be9b8d8a38e27841edf4a00aee029
Brent-the-carpenter <beh337@gmail.com>
2024-08-06 21:00:21 -0400

    I have effectively converted the MongoDb schemas into PostgreSQL table schemas.

--- db/schemas/tables.sql
+++ db/schemas/tables.sql
@@ -18,3 +18,31 @@ CREATE TABLE materials (
     img_id TEXT ,
     FOREIGN KEY (category_id) REFERENCES categories(id)
 );
+-- Create enum type for days of the week
+CREATE TYPE day_of_week AS ENUM(
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday') ; 
+
+-- Create location table 
+CREATE TABLE locations (
+    id PRIMARY KEY GENERATED ALWAYS AS IDENTITY , 
+    state VARCHAR(20) NOT NULL   , 
+    address VARCHAR(255) NOT NULL ,
+    phone_number VARCHAR(15) ,
+     open day_of_week[], 
+    zip_code VARCHAR(20) NOT NULL ,
+    store_id INT NOT NULL ,
+    FOREIGN KEY (store_id) REFERENCES stores(id) 
+);
+
+-- Create store table 
+CREATE TABLE stores(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY , 
+    name VARCHAR(50) NOT NULL , 
+    date_opened DATE NOT NULL  
+)
